673784413d6335702d95ef019b7ad12c036b4bb9
Yuggi <1921411221@qq.com>
2019-03-02 21:00:20 +0800

    页面修改为angularjs

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,45 @@
+var travelApp = angular.module('travelApp',['ui.router']);
+
+/**
+ * 由于整个应用都会和路由打交道，所以这里把$state和$stateParams这两个对象放到$rootScope上，方便其它地方引用和注入。
+ * 这里的run方法只会在angular启动的时候运行一次。
+ * @param  {[type]} $rootScope
+ * @param  {[type]} $state
+ * @param  {[type]} $stateParams
+ * @return {[type]}
+ */
+travelApp.run(function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
+
+/**
+ * 配置路由。
+ * 注意这里采用的是ui-router这个路由，而不是ng原生的路由。
+ * ng原生的路由不能支持嵌套视图，所以这里必须使用ui-router。
+ * @param  {[type]} $stateProvider
+ * @param  {[type]} $urlRouterProvider
+ * @return {[type]}
+ */
+travelApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/index');
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            views: {
+                '': {
+                    templateUrl: 'html/app.html'
+                },
+                'header@index': {
+                    templateUrl: 'html/template/header.html'
+                },
+                'body@index': {
+                    templateUrl: 'html/body.html'
+                },
+                'footer@index': {
+                    templateUrl: 'html/template/footer.html'
+                }
+            }
+        })
+       
+});
