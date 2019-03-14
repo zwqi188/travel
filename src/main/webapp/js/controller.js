9f5230a0bac04c3a43bde3a20898a2f1b36392f3
Yuggi <1921411221@qq.com>
2019-03-14 22:34:54 +0800

    添加服务

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,24 +1,27 @@
-travelApp.controller('registerController', function($scope, $http, $state, $stateParams) {
+travelApp.controller('registerController', function($scope, $http, $state, $stateParams, Http) {
     console.log($stateParams);
 
     $scope.registerFromServer = function () {
-        console.log("registerFromServer");
-        console.log($stateParams);
-        $http({
-            method:'POST',
-            url:'/travel/register.json',
-            data:{'mobile':'123143','password':'12345'}
-        }).success(function(data){
-            alert(data.respMsg);
-        }).error(function(data, status, headers, config){
-            alert(data.respMsg);
-        });
+        console.log($scope.mobile);
+        var param = {
+            'mobile':$scope.mobile,
+            'password':$scope.password
+        };
+        Http.go($http,
+            'POST',
+            '/travel/register.json',
+            param,
+            function(data){alert(data.respMsg)},
+            function(data){alert(data.respMsg)});
     }
+
     $scope.getFromServer = function () {
-        $http('/travel/get.json')
-            .success(function (data) {
-                console.log(data);
-            });
+        Http.go($http,
+            'GET',
+            '/travel/get.json',
+            null,
+            function(data){alert(data)},
+            function(data){alert(data)});
     }
 
 });
