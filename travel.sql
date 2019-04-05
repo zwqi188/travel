46645ec0748e048413fecfc5e6a5e250404565cd
Yuggi <1921411221@qq.com>
2019-04-05 17:05:31 +0800

    提交代码，修改bug

--- travel.sql
+++ travel.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50641
  File Encoding         : utf-8
 
- Date: 03/24/2019 15:09:52 PM
+ Date: 04/05/2019 16:49:37 PM
 */
 
 SET NAMES utf8;
@@ -79,6 +79,32 @@ CREATE TABLE `discount` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+--  Table structure for `menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menuId` varchar(255) DEFAULT NULL,
+  `menuUri` varchar(255) DEFAULT NULL,
+  `menuThumbnail` varchar(255) DEFAULT NULL,
+  `menuOrder` int(11) DEFAULT NULL,
+  `isLeaf` int(50) DEFAULT NULL,
+  `parentId` int(11) DEFAULT NULL,
+  `menuName` varchar(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `state` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `menu`
+-- ----------------------------
+BEGIN;
+INSERT INTO `menu` VALUES ('1', '1001', 'order', 'fa-list-alt', '1', '0', '0', '用户管理', null, null, '有效'), ('2', '1002', 'payInfo', 'fa-dot-circle-o', '2', '0', '0', '景点管理', null, null, '有效'), ('3', '1003', 'order2', 'fa-dot-circle-o', '3', '0', '0', '订单管理', null, null, '有效'), ('4', '1004', 'order2', 'fa-dot-circle-o', '4', '0', '0', '评论管理', null, null, '有效'), ('5', '100101', 'order.payment', null, '0', '1', '1', '订单支付情况查询', null, null, '有效'), ('6', '100102', 'order.paymentDetail', null, '1', '1', '1', '支付详情情况查询', null, null, '有效');
+COMMIT;
+
+-- ----------------------------
 --  Table structure for `order`
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
@@ -136,7 +162,6 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userName` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `realName` varchar(25) DEFAULT NULL,
   `mobile` varchar(11) DEFAULT NULL,
   `email` varchar(25) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
@@ -146,6 +171,7 @@ CREATE TABLE `user` (
   `address` varchar(255) DEFAULT NULL,
   `idCard` varchar(25) DEFAULT NULL,
   `discountId` int(11) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
@@ -153,32 +179,7 @@ CREATE TABLE `user` (
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('1', null, '12345', null, '123143', null, '2019-03-14 19:40:46', null, null, null, null, null, null), ('2', null, '1231', null, '123213', null, '2019-03-14 21:54:13', null, null, null, null, null, null), ('3', null, '324', null, 'er', null, '2019-03-14 21:58:48', null, null, null, null, null, null), ('4', null, '9', null, '909', null, '2019-03-14 22:04:05', null, null, null, null, null, null), ('5', null, '534', null, '234', null, '2019-03-14 22:29:20', null, null, null, null, null, null), ('6', null, '6464', null, '6564645', null, '2019-03-16 10:50:41', null, null, null, null, null, null), ('7', null, '202cb962ac59075b964b07152d234b70', null, '32423435234', null, '2019-03-16 14:31:09', null, null, null, null, null, null), ('8', null, '202cb962ac59075b964b07152d234b70', null, '43425426543', null, '2019-03-16 14:32:39', null, null, null, null, null, null), ('9', null, '202cb962ac59075b964b07152d234b70', null, '42342342543', null, '2019-03-16 14:33:35', null, null, null, null, null, null), ('10', null, '202cb962ac59075b964b07152d234b70', null, '35324523626', null, '2019-03-16 14:34:15', null, null, null, null, null, null), ('11', null, '202cb962ac59075b964b07152d234b70', null, '53453453453', null, '2019-03-16 14:35:04', null, null, null, null, null, null), ('12', null, '202cb962ac59075b964b07152d234b70', null, '53453454645', null, '2019-03-16 14:50:15', null, null, null, null, null, null), ('13', null, 'e10adc3949ba59abbe56e057f20f883e', null, '12464565476', null, '2019-03-16 15:10:04', null, null, null, null, null, null), ('14', '张三', 'e10adc3949ba59abbe56e057f20f883e', null, '18888888888', null, '2019-03-16 16:31:49', '2019-03-23 12:27:48', '大帅哥', null, '{\"province\":\"内蒙古\",\"city\":\"通辽\",\"area\":\"奈曼旗\",\"detail\":\"hg\"}', '67899798', null), ('15', null, 'e10adc3949ba59abbe56e057f20f883e', null, '12222222222', null, '2019-03-17 14:47:35', null, null, null, null, null, null);
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
--- ----------------------------
---  Table structure for `menu`
--- ----------------------------
-DROP TABLE IF EXISTS `menu`;
-CREATE TABLE `menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `menuId` varchar(255) DEFAULT NULL,
-  `menuUri` varchar(255) DEFAULT NULL,
-  `menuThumbnail` varchar(255) DEFAULT NULL,
-  `menuOrder` int(11) DEFAULT NULL,
-  `isLeaf` int(50) DEFAULT NULL,
-  `parentId` int(11) DEFAULT NULL,
-  `menuName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
--- ----------------------------
---  Records of `menu`
--- ----------------------------
-BEGIN;
-INSERT INTO `menu` VALUES ('1', '1001', 'order', 'fa-list-alt', '1', '0', '0', '用户管理'), ('2', '1002', 'payInfo', 'fa-dot-circle-o', '2', '0', '0', '景点管理'), ('3', '1003', 'order2', 'fa-dot-circle-o', '3', '0', '0', '订单管理'), ('4', '1004', 'order2', 'fa-dot-circle-o', '4', '0', '0', '评论管理'), ('5', '100101', 'order.payment', null, '0', '1', '1', '订单支付情况查询'), ('6', '100102', 'order.paymentDetail', null, '1', '1', '1', '支付详情情况查询');
+INSERT INTO `user` VALUES ('1', null, '12345', '123143', null, '2019-03-14 19:40:46', null, null, null, null, null, null, '有效'), ('2', null, '1231', '123213', null, '2019-03-14 21:54:13', null, null, null, null, null, null, '有效'), ('3', null, '324', 'er', null, '2019-03-14 21:58:48', null, null, null, null, null, null, '有效'), ('4', null, '9', '909', null, '2019-03-14 22:04:05', null, null, null, null, null, null, '有效'), ('5', null, '534', '234', null, '2019-03-14 22:29:20', null, null, null, null, null, null, '有效'), ('6', null, '6464', '6564645', null, '2019-03-16 10:50:41', null, null, null, null, null, null, null), ('7', null, '202cb962ac59075b964b07152d234b70', '32423435234', null, '2019-03-16 14:31:09', null, null, null, null, null, null, null), ('8', null, '202cb962ac59075b964b07152d234b70', '43425426543', null, '2019-03-16 14:32:39', null, null, null, null, null, null, null), ('9', null, '202cb962ac59075b964b07152d234b70', '42342342543', null, '2019-03-16 14:33:35', null, null, null, null, null, null, null), ('10', null, '202cb962ac59075b964b07152d234b70', '35324523626', null, '2019-03-16 14:34:15', null, null, null, null, null, null, null), ('11', null, '202cb962ac59075b964b07152d234b70', '53453453453', null, '2019-03-16 14:35:04', null, null, null, null, null, null, null), ('12', null, '202cb962ac59075b964b07152d234b70', '53453454645', null, '2019-03-16 14:50:15', null, null, null, null, null, null, null), ('13', null, 'e10adc3949ba59abbe56e057f20f883e', '12464565476', null, '2019-03-16 15:10:04', null, null, null, null, null, null, null), ('14', '张三', 'e10adc3949ba59abbe56e057f20f883e', '18888888888', null, '2019-03-16 16:31:49', '2019-03-23 12:27:48', '大帅哥', null, '{\"province\":\"内蒙古\",\"city\":\"通辽\",\"area\":\"奈曼旗\",\"detail\":\"hg\"}', '67899798', null, '有效'), ('15', null, 'e10adc3949ba59abbe56e057f20f883e', '12222222222', null, '2019-03-17 14:47:35', null, null, null, null, null, null, null);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
